0bdb2125dec3f0cf823a273ac257c9db982f06bf
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-03-15 16:29:33 -0300

    feat: cria classe estatica para validação de token

---
+++ JwtTokenApp.Api/Autenticacao/TokenHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JwtTokenApp.Api.Autenticacao;
+
+public static class TokenHelper
+{
+    public static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]!));
+
+        return new TokenValidationParameters()
+        {
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidateAudience = true,
+            ValidAudience = configuration["JwtSettings:Audience"],
+            ValidateIssuer = true,
+            ValidIssuer = configuration["JwtSettings:Issuer"],
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key
+        };
+    }
+}
