cb62040e403a9f4a76ab793478d686d348bc5eda
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-03-15 16:30:39 -0300

    refactor: move logica de validação de token para TokenHelper

--- JwtTokenApp.Api/Program.cs
+++ JwtTokenApp.Api/Program.cs
@@ -14,19 +14,7 @@ builder.Services.AddAuthentication(options =>
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(options =>
 {
-    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]!));
-
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.Zero,
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["JwtSettings:Audience"],
-        ValidateIssuer = true,
-        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = key
-    };
+    options.TokenValidationParameters = TokenHelper.GetTokenValidationParameters(builder.Configuration);
 });
 
 builder.Services.AddAuthorization();
